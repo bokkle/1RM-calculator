f529249041d8df29f4f5889a7edae77460f60635
bokkle <mitch.r_@hotmail.com>
2024-02-01 20:46:41 -0500

    updates

--- src/SelectLift.js
+++ src/SelectLift.js
@@ -1,20 +1,19 @@
 const SelectLift = ({ onHandleSetLift, lift }) => {
   return (
-    <div style={{ textAlign: "center" }}>
+    // <div style={{ textAlign: "center" }}>
       <select
         onChange={(e) => onHandleSetLift(e)}
         value={lift}
-        style={{ margin: "auto" }}
       >
         <option value="" disabled hidden>
-          Select Exercise
+          Select Lift
         </option>
         <option value="Bench">Bench Press</option>
         <option value="Squat">Squat</option>
         <option value="Deadlift">Deadlift</option>
         <option value="OHP">Overhead Press</option>
       </select>
-    </div>
+    // </div>
   );
 };
 
